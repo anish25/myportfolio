39004f27397bb165fc201122205ea32b9cddfa5e
anish25 <anishgandhi25@gmail.com>
2019-08-15 16:07:49 -0400

    commit third

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Anish Gandhi' });
 });
 
+/* GET projects page. */
 router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projets' });
 });
 
+/* GET services page. */
 router.get('/services', function(req, res, next) {
   res.render('services', { title: 'Services' });
 });
 
+/* GET about page. */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
 
+/* GET contact page. */
 router.get('/Contact', function(req, res, next) {
   res.render('Contact', { title: 'Contact' });
 });
